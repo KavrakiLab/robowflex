55d1632d8d072ee302577c6933369a9ac5e08f49
Andrew Wells <andrew.m.wells@nasa.gov>
2018-06-12 15:44:35 -0500

    r2 runs in walking

--- tmpack/src/main.cpp
+++ tmpack/src/main.cpp
@@ -16,40 +16,30 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "roboflex", ros::init_options::NoSigintHandler);
     signal(SIGINT, shutdown);
 
-    Robot ur5("ur5");
-    ur5.initialize("package://ur_description/urdf/ur5_robotiq_robot_limited.urdf.xacro",  // urdf
-                   "package://ur5_robotiq85_moveit_config/config/ur5_robotiq85.srdf",     // srdf
-                   "package://ur5_robotiq85_moveit_config/config/joint_limits.yaml",      // joint limits
-                   "package://ur5_robotiq85_moveit_config/config/kinematics.yaml"         // kinematics
+    Robot r2("r2");
+
+    r2.initialize("package://r2_description/urdf/r2c6.urdf",  // urdf
+                   "package://r2_moveit_config/config/r2.srdf",     // srdf
+                   "package://r2_moveit_config/config/joint_limits.yaml",      // joint limits
+                   "package://r2_moveit_config/config/kinematics.yaml"         // kinematics
     );
 
-    Scene scene(ur5);
+    Scene scene(r2);
 
-    OMPL::OMPLPipelinePlanner planner(ur5);
-    planner.initialize("package://ur5_robotiq85_moveit_config/config/ompl_planning.yaml"  // planner config
+    OMPL::OMPLPipelinePlanner planner(r2);
+    planner.initialize("package://r2_moveit_config/config/ompl_planning.yaml"  // planner config
     );
 
-    // OMPL::OMPLInterfacePlanner planner(ur5);
-    // planner.initialize("package://ur5_robotiq85_moveit_config/config/ompl_planning.yaml"  // planner config
-    // );
-
-    // Geometry box(Geometry::ShapeType::BOX, {0.1, 0.1, 0.1});
-    // Eigen::Affine3d pose = Eigen::Affine3d::Identity();
-    // pose.translate(Eigen::Vector3d{1, 1, 1});
-
-    // scene.addCollisionObject("box", box, pose);
-
-    MotionRequestBuilder request(planner, "manipulator");
-    request.setStartConfiguration({0.0677, -0.8235, 0.9860, -0.1624, 0.0678, 0.0});
-    request.setGoalConfiguration({-0.39, -0.69, -2.12, 2.82, -0.39, 0.0});
-
-    std::vector<double> start = {0.0677, -0.8235, 0.9860, -0.1624, 0.0678, 0.0};
-    MyWalker walker(ur5, "manipulator", planner, scene, request, start);
+    MotionRequestBuilder request(planner, "legsandtorso");
+    request.setStartConfiguration({0,0,0,0,0,0,1,0,0,8.88178e-16,1.5708,8.88178e-16,1.5708,1.5708,0,0,0,0,8.88178e-16,0,1.5708,0,1.5708,1.5708,0,0,0,0,0.872665,-1.39626,-1.8326,-2.44346,1.39626,0,0,8.88178e-16,0,8.88178e-16,8.88178e-16,0,8.88178e-16,0,0,8.88178e-16,8.88178e-16,-8.88178e-16,8.88178e-16,8.88178e-16,0,1.77636e-15,1.77636e-15,-1.77636e-15,0,-0.872665,-1.39626,1.8326,-2.44346,-1.39626,0,8.88178e-16,0,0,0,-8.88178e-16,0,8.88178e-16,0,0,-8.88178e-16,0,0,8.88178e-16,0,8.88178e-16,1.77636e-15,0,-8.88178e-16,0,-0.0872665,8.88178e-16,8.88178e-16});
+    request.setGoalConfiguration({0,0,0,0,0,0,1,0,0,8.88178e-16,1.5708,8.88178e-16,1.5708,1.5708,0,0,0,0,8.88178e-16,0,1.5708,0,1.5708,1.5708,0,0,0,0,0.872665,-1.39626,-1.8326,-2.44346,1.39626,0,0,8.88178e-16,0,8.88178e-16,8.88178e-16,0,8.88178e-16,0,0,8.88178e-16,8.88178e-16,-8.88178e-16,8.88178e-16,8.88178e-16,0,1.77636e-15,1.77636e-15,-1.77636e-15,0,-0.872665,-1.39626,1.8326,-2.44346,-1.39626,0,8.88178e-16,0,0,0,-8.88178e-16,0,8.88178e-16,0,0,-8.88178e-16,0,0,8.88178e-16,0,8.88178e-16,1.77636e-15,0,-8.88178e-16,0,-0.0772665,8.88178e-16,8.88178e-16});
 
     ros::Rate rate(0.5);
     while (ros::ok())
     {
-        auto res = walker.plan();
+        auto res = planner.plan(scene, request.getRequest());
+        if (res.error_code_.val != moveit_msgs::MoveItErrorCodes::SUCCESS)
+            break;
 
         // rviz.update(res);
 
