de46fc5f368cdc0ce72b4a2efa58e0f22db6e72e
Andrew Wells <andrew.m.wells@nasa.gov>
2018-06-14 14:26:39 -0500

    skeleton of idea for passing constraints and scene graph changes through the tmp interface

--- tmpack/src/main.cpp
+++ tmpack/src/main.cpp
@@ -4,7 +4,16 @@
 #include <signal.h>
 
 #define NUM_ITERATIONS 1
-#define START_POSE {1.98552, 0.0730371, 9.14127e-05, 4.8366e-06, -2.4964e-06, 1, -6.53607e-07, 0, 0, 0, 1.5708, 8.88178e-16, 1.5708, 1.5708, 0, 0, 0, 0, 0, -8.88178e-16, 1.5708, -8.88178e-16, 1.5708, 1.5708, 0, 0, 0, 0, 0.872665, -1.39626, -1.8326, -2.44346, 1.39626, 0, 0, 8.88178e-16, 0, -8.88178e-16, 8.88178e-16, 0, 8.88178e-16, -8.88178e-16, 0, 8.88178e-16, 0, 8.88178e-16, 8.88178e-16, 8.88178e-16, 0, 8.88178e-16, 0, 0, 0, -0.872665, -1.39626, 1.8326, -2.44346, -1.39626, 0, -8.88178e-16, 0, 0, 0, 0, 0, -8.88178e-16, 0, 0, 0, 8.88178e-16, 0, 0, 0, -8.88178e-16, 8.88178e-16, -8.88178e-16, -4.44089e-15, 1.77636e-15, -0.0872665, 1.77636e-15, 0}
+#define START_POSE                                                                                                     \
+    {                                                                                                                  \
+        1.98552, 0.0730371, 9.14127e-05, 4.8366e-06, -2.4964e-06, 1, -6.53607e-07, 0, 0, 0, 1.5708, 8.88178e-16,       \
+            1.5708, 1.5708, 0, 0, 0, 0, 0, -8.88178e-16, 1.5708, -8.88178e-16, 1.5708, 1.5708, 0, 0, 0, 0, 0.872665,   \
+            -1.39626, -1.8326, -2.44346, 1.39626, 0, 0, 8.88178e-16, 0, -8.88178e-16, 8.88178e-16, 0, 8.88178e-16,     \
+            -8.88178e-16, 0, 8.88178e-16, 0, 8.88178e-16, 8.88178e-16, 8.88178e-16, 0, 8.88178e-16, 0, 0, 0,           \
+            -0.872665, -1.39626, 1.8326, -2.44346, -1.39626, 0, -8.88178e-16, 0, 0, 0, 0, 0, -8.88178e-16, 0, 0, 0,    \
+            8.88178e-16, 0, 0, 0, -8.88178e-16, 8.88178e-16, -8.88178e-16, -4.44089e-15, 1.77636e-15, -0.0872665,      \
+            1.77636e-15, 0                                                                                             \
+    }
 
 using namespace robowflex;
 
@@ -21,27 +30,28 @@ int main(int argc, char **argv)
 
     Robot r2("r2");
 
-    r2.initialize("package://r2_description/urdf/r2c6.urdf",  // urdf
-                   "package://r2_moveit_config/config/r2.srdf",     // srdf
-                   "package://r2_moveit_config/config/joint_limits.yaml",      // joint limits
-                   "package://r2_moveit_config/config/kinematics.yaml"         // kinematics
+    r2.initialize("package://r2_description/urdf/r2c6.urdf",              // urdf
+                  "package://r2_moveit_config/config/r2.srdf",            // srdf
+                  "package://r2_moveit_config/config/joint_limits.yaml",  // joint limits
+                  "package://r2_moveit_config/config/kinematics.yaml"     // kinematics
     );
 
     Scene scene(r2);
 
-    //add the handrails
+    // add the handrails
     // scene.updateCollisionObject("ISS Handrail 0", ,);
-
 
     OMPL::OMPLPipelinePlanner planner(r2);
     planner.initialize("package://r2_moveit_config/config/ompl_planning.yaml",  // planner config
-                       OMPL::Settings(), // settings
-                       "ompl_interface/OMPLPlanningContextManager" // plugin
+                       OMPL::Settings(),                                        // settings
+                       "ompl_interface/OMPLPlanningContextManager"              // plugin
     );
 
     MotionRequestBuilder request(planner, "legsandtorso");
-    //Lock the right foot in place:
-    request.addPathPositionConstraint("r2/right_leg/gripper/tip", "/world", Eigen::Affine3d(Eigen::Translation3d(1.983001, 0.321150, -1.361)), Geometry(Geometry::ShapeType::Type::CYLINDER, Eigen::Vector3d(0.01,0.01,0.01), "right_foot_base_position"));
+    // Lock the right foot in place:
+    request.addPathPositionConstraint(
+        "r2/right_leg/gripper/tip", "/world", Eigen::Affine3d(Eigen::Translation3d(1.983001, 0.321150, -1.361)),
+        Geometry(Geometry::ShapeType::Type::SPHERE, Eigen::Vector3d(0.01, 0.01, 0.01), "right_foot_base_position"));
 
     std::vector<double> start = START_POSE;
 
@@ -58,16 +68,16 @@ int main(int argc, char **argv)
         size_t begin = ros::Time::now().nsec;
         auto res = walker.plan();
         if (res[0].error_code_.val != moveit_msgs::MoveItErrorCodes::SUCCESS)
-             success_count++;
+            success_count++;
 
         // rviz.update(res);
 
-        time_spent += (ros::Time::now().nsec-begin);
+        time_spent += (ros::Time::now().nsec - begin);
         ros::spinOnce();
         rate.sleep();
     }
 
-    std::cout<<"Time spent: "<<time_spent<<std::endl;
+    std::cout << "Time spent: " << time_spent << std::endl;
 
     return 0;
 }
