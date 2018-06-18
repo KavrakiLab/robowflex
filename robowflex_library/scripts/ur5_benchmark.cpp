ab9c82cc3d6087383cadb055e2c8c7bd1487f4bd
Zachary Kingston <zak@rice.edu>
2018-06-18 11:53:51 -0500

    multiple request benching

--- robowflex_library/scripts/ur5_benchmark.cpp
+++ robowflex_library/scripts/ur5_benchmark.cpp
@@ -29,24 +29,29 @@ int main(int argc, char **argv)
     planner.initialize("package://ur5_robotiq85_moveit_config/config/ompl_planning.yaml"  // planner config
     );
 
-    MotionRequestBuilder request(planner, "manipulator");
-    request.setStartConfiguration({0.0677, -0.8235, 0.9860, -0.1624, 0.0678, 0.0});
+    MotionRequestBuilder joint_request(planner, "manipulator");
+    joint_request.setStartConfiguration({0.0677, -0.8235, 0.9860, -0.1624, 0.0678, 0.0});
+    joint_request.setGoalConfiguration({-0.39, -0.69, -2.12, 2.82, -0.39, 0});
+
+    MotionRequestBuilder pose_request(planner, "manipulator");
+    pose_request.setStartConfiguration({0.0677, -0.8235, 0.9860, -0.1624, 0.0678, 0.0});
 
     Eigen::Affine3d pose = Eigen::Affine3d::Identity();
     pose.translate(Eigen::Vector3d{-0.268, -0.826, 1.313});
     Eigen::Quaterniond orn{0, 0, 1, 0};
 
-    request.setGoalRegion("ee_link", "world",                                         // links
-                          pose, Geometry(Geometry::ShapeType::SPHERE, {0.01, 0, 0}),  // position
-                          orn, {0.01, 0.01, 0.01}                                     // orientation
+    pose_request.setGoalRegion("ee_link", "world",                                         // links
+                               pose, Geometry(Geometry::ShapeType::SPHERE, {0.01, 0, 0}),  // position
+                               orn, {0.01, 0.01, 0.01}                                     // orientation
     );
 
     ur5.loadKinematics("manipulator");
 
     Benchmarker benchmark;
-    benchmark.addBenchmarkingRequest("test", scene, planner, request);
+    benchmark.addBenchmarkingRequest("joint", scene, planner, joint_request);
+    benchmark.addBenchmarkingRequest("pose", scene, planner, pose_request);
 
-    OMPLBenchmarkOutputter out("test.log");
+    OMPLBenchmarkOutputter out("robowflex_ur5_test/");
     benchmark.benchmark(out);
 
     return 0;
