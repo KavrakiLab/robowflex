dd15de6f695eae2ed9b6bbcab6a712ef38947a2c
Zachary Kingston <zachary.k.kingston@nasa.gov>
2018-06-19 12:35:36 -0500

    Added ur5 detail

---
+++ robowflex_library/src/detail/ur5.cpp
@@ -0,0 +1,34 @@
+#include <robowflex_library/robowflex.h>
+#include <robowflex_library/detail/ur5.h>
+
+
+using namespace robowflex;
+
+const std::string UR5Robot::URDF{"package://ur_description/urdf/ur5_robotiq_robot_limited.urdf.xacro"};
+const std::string UR5Robot::SRDF{"package://ur5_robotiq85_moveit_config/config/ur5_robotiq85.srdf"};
+const std::string UR5Robot::LIMITS{"package://ur5_robotiq85_moveit_config/config/joint_limits.yaml"};
+const std::string UR5Robot::KINEMATICS{"package://ur5_robotiq85_moveit_config/config/kinematics.yaml"};
+
+const std::string OMPL::UR5OMPLPipelinePlanner::CONFIG{"package://ur5_robotiq85_moveit_config/config/ompl_planning.yaml"};
+
+UR5Robot::UR5Robot() : Robot("ur5")
+{
+}
+
+bool UR5Robot::initialize()
+{
+    bool success = Robot::initialize(URDF, SRDF, LIMITS, KINEMATICS);
+    loadKinematics("manipulator");
+
+    return success;
+}
+
+OMPL::UR5OMPLPipelinePlanner::UR5OMPLPipelinePlanner(UR5Robot &robot) : OMPLPipelinePlanner(robot)
+{
+}
+
+bool OMPL::UR5OMPLPipelinePlanner::initialize(const std::string &config_file, const Settings settings,
+                                             const std::string &plugin, const std::vector<std::string> &adapters)
+{
+    return OMPLPipelinePlanner::initialize(config_file, settings, plugin, adapters);
+}
