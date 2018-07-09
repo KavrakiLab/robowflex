f288f8848d176e9e6b7914d16b00ed14b78c9da6
Zachary Kingston <zak@rice.edu>
2018-07-09 10:54:54 -0500

    Added HDF5 cmake

---
+++ robowflex_library/include/robowflex_library/io/hdf5.h
@@ -0,0 +1,16 @@
+/* Author: Zachary Kingston */
+
+#ifndef ROBOWFLEX_IO_HDF5_
+#define ROBOWFLEX_IO_HDF5_
+
+namespace robowflex
+{
+    namespace IO
+    {
+        class HDF5File
+        {
+        };
+    }  // namespace IO
+}  // namespace robowflex
+
+#endif
---
+++ robowflex_library/src/io/hdf5.cpp
@@ -0,0 +1,7 @@
+/* Author: Zachary Kingston */
+
+#include "H5Cpp.h"
+
+#include <robowflex_library/io/hdf5.h>
+
+using namespace H5;
