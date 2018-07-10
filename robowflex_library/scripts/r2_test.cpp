018eeecb212ff5349abbee3555d72aa3d911c0bc
Andrew Wells <andrew.m.wells@nasa.gov>
2018-07-10 15:43:18 -0500

    clang format and attempts to add torso constraint

--- robowflex_library/scripts/r2_test.cpp
+++ robowflex_library/scripts/r2_test.cpp
@@ -40,20 +40,33 @@ int main(int argc, char **argv)
     const std::string world = "world";
     const std::string waist = "r2/waist_center";
     const std::string left_foot = "r2/left_leg/gripper/tip";
-    Eigen::Vector3d tolerances(0.01, 0.01, 0.01);
+    const std::string right_foot = "r2/right_leg/gripper/tip";
+
+    Eigen::Vector3d tolerance_feet(0.01, 0.01, 0.01);
+    Eigen::Vector3d tolerance_waist(0.005, 0.005, 0.005);
 
     // Set a pose constraint on the left foot (keep fixed throughout the path).
     auto foot_tf = r2->getLinkTF(left_foot);
     request.addPathPoseConstraint(           //
         left_foot, world,                    //
-        foot_tf, Geometry::makeSphere(0.1),  //
-        Eigen::Quaterniond(foot_tf.rotation()), tolerances);
+        foot_tf, Geometry::makeSphere(0.01),  //
+        Eigen::Quaterniond(foot_tf.rotation()), tolerance_feet);
 
     // Set a orientation constraint on the waist (to keep it up throughout the path)
     auto waist_tf = r2->getRelativeLinkTF(left_foot, waist);
     request.addPathOrientationConstraint(  //
         waist, left_foot,                  //
-        Eigen::Quaterniond(waist_tf.rotation()), tolerances);
+        Eigen::Quaterniond(waist_tf.rotation()), tolerance_waist);
+
+
+    //should be the same goal as the yaml file
+    Eigen::Affine3d goal_tf = Eigen::Translation3d(1.80676028419, -0.248108850885, -1.10411526908) * Eigen::Quaterniond::Identity();
+    request.setGoalRegion(right_foot, world,
+                                         goal_tf, Geometry::makeSphere(0.01),
+                                         Eigen::Quaterniond(-0.999999999961, 1.82668011027e-06, 7.14501707513e-06, 4.90351543079e-06), tolerance_feet);
+    // request.setGoalRegion(right_foot, world,
+    //                              goal_tf, Geometry::makeSphere(0.1),
+    //                              Eigen::Quaterniond(0, 0, 1, 0), tolerances);
 
     // Do motion planning!
     planning_interface::MotionPlanResponse res = planner->plan(scene, request.getRequest());
--- tmpack/src/footstep_planner/utils/geom_2D.h
+++ tmpack/src/footstep_planner/utils/geom_2D.h
@@ -4,48 +4,58 @@
 #include <math.h>
 #include <iostream>
 
-namespace footstep_planning {
-class point_2D {
-   public:
-    double x, y;
+namespace footstep_planning
+{
+    class point_2D
+    {
+    public:
+        double x, y;
 
-    point_2D(double x, double y) {
-        this->x = x;
-        this->y = y;
+        point_2D(double x, double y)
+        {
+            this->x = x;
+            this->y = y;
+        }
+
+        friend std::ostream &operator<<(std::ostream &strm, const point_2D &p)
+        {
+            return strm << "<" << p.x << ", " << p.y << ">";
+        }
+
+        friend bool operator==(const point_2D &p1, const point_2D &p2)
+        {
+            return p1.x == p2.x && p1.y == p2.y;
+        }
+
+        friend bool operator!=(const point_2D &p1, const point_2D &p2)
+        {
+            return ~(p1 == p2);
+        }
+    };
+
+    class line_segment
+    {
+    public:
+        double x1, y1, x2, y2;
+
+        line_segment(double x1, double y1, double x2, double y2)
+        {
+            this->x1 = x1;
+            this->x2 = x2;
+            this->y1 = y1;
+            this->y2 = y2;
+        }
+    };
+
+    double dist(double x1, double y1, double x2, double y2)
+    {
+        return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
 
-    friend std::ostream &operator<<(std::ostream &strm, const point_2D &p) {
-        return strm << "<" << p.x << ", " << p.y << ">";
+    double dist(point_2D p1, point_2D p2)
+    {
+        return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
     }
-
-    friend bool operator==(const point_2D &p1, const point_2D &p2) {
-        return p1.x == p2.x && p1.y == p2.y;
-    }
-
-    friend bool operator!=(const point_2D &p1, const point_2D &p2) {
-        return ~(p1 == p2);
-    }
-};
-
-class line_segment {
-   public:
-    double x1, y1, x2, y2;
-
-    line_segment(double x1, double y1, double x2, double y2) {
-        this->x1 = x1;
-        this->x2 = x2;
-        this->y1 = y1;
-        this->y2 = y2;
-    }
-};
-
-double dist(double x1, double y1, double x2, double y2) {
-    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-}
-
-double dist(point_2D p1, point_2D p2) {
-    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-}
 
 }  // namespace footstep_planning
 #endif
