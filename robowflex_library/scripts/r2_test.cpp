3eb09fe24c6bb5c390cbe6c91efa2bd291cfe33e
Zachary Kingston <zachary.k.kingston@nasa.gov>
2018-07-10 10:57:16 -0500

    Added robot state functions + r2 test example

--- robowflex_library/scripts/r2_test.cpp
+++ robowflex_library/scripts/r2_test.cpp
@@ -1,4 +1,5 @@
 #include <robowflex_library/util.h>
+#include <robowflex_library/geometry.h>
 #include <robowflex_library/robot.h>
 #include <robowflex_library/scene.h>
 #include <robowflex_library/planning.h>
@@ -28,8 +29,28 @@ int main(int argc, char **argv)
     // Load a motion planning request (a step with a torso constraint).
     MotionRequestBuilder request(planner, "legsandtorso");
     request.fromYAMLFile("package://robowflex_library/yaml/r2_plan_waist.yml");
+    r2->setState(request.getRequest().start_state);
 
-   // Do motion planning!
+    request.getPathConstraints().position_constraints.clear();
+    request.getPathConstraints().orientation_constraints.clear();
+
+    // Leftfoot tip
+    const std::string waist = "r2/waist_center";
+    const std::string left_foot = "r2/left_leg/gripper/tip";
+    Eigen::Vector3d tolerances(0.01, 0.01, 0.01);
+
+    auto foot_tf = r2->getLinkTF(left_foot);
+    request.addPathPoseConstraint(
+        left_foot, "world",  //
+        foot_tf, std::make_shared<Geometry>(Geometry::ShapeType::SPHERE, Eigen::Vector3d(0.1, 0.1, 0.1)),
+        Eigen::Quaterniond(foot_tf.rotation()), tolerances);
+
+    // Waist up
+    auto waist_tf = r2->getRelativeLinkTF(left_foot, waist);
+    request.addPathOrientationConstraint(waist, left_foot,  //
+                                         Eigen::Quaterniond(waist_tf.rotation()), tolerances);
+
+    // Do motion planning!
     planning_interface::MotionPlanResponse res = planner->plan(scene, request.getRequest());
     if (res.error_code_.val != moveit_msgs::MoveItErrorCodes::SUCCESS)
         return 1;
