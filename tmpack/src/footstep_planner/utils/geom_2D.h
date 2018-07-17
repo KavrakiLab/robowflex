3fae3671c9af31e04872b788071eff8629d42aad
Andrew Wells <andrew.m.wells@nasa.gov>
2018-07-17 15:33:30 -0500

    task planner changes

--- tmpack/src/footstep_planner/utils/geom_2D.h
+++ tmpack/src/footstep_planner/utils/geom_2D.h
@@ -4,74 +4,58 @@
 #include <math.h>
 #include <iostream>
 
-namespace footstep_planning
-{
-    class point_2D
-    {
-    public:
-        double x, y;
+namespace footstep_planning {
+class point_2D {
+ public:
+  double x, y;
 
-        point_2D(double x, double y)
-        {
-            this->x = x;
-            this->y = y;
-        }
+  point_2D(double x, double y) {
+    this->x = x;
+    this->y = y;
+  }
 
-        friend std::ostream &operator<<(std::ostream &strm, const point_2D &p)
-        {
-            return strm << "<" << p.x << ", " << p.y << ">";
-        }
+  friend std::ostream &operator<<(std::ostream &strm, const point_2D &p) {
+    return strm << "<" << p.x << ", " << p.y << ">";
+  }
 
-        friend bool operator==(const point_2D &p1, const point_2D &p2)
-        {
-            return p1.x == p2.x && p1.y == p2.y;
-        }
+  friend bool operator==(const point_2D &p1, const point_2D &p2) {
+    return p1.x == p2.x && p1.y == p2.y;
+  }
 
-        friend bool operator!=(const point_2D &p1, const point_2D &p2)
-        {
-            return ~(p1 == p2);
-        }
-    };
+  friend bool operator!=(const point_2D &p1, const point_2D &p2) {
+    return ~(p1 == p2);
+  }
+};
 
-    class line_segment
-    {
-    public:
-        double x1, y1, x2, y2;
+class line_segment {
+ public:
+  double x1, y1, x2, y2;
 
-        line_segment(double x1, double y1, double x2, double y2)
-        {
-            this->x1 = x1;
-            this->x2 = x2;
-            this->y1 = y1;
-            this->y2 = y2;
-        }
-    };
+  line_segment(double x1, double y1, double x2, double y2) {
+    this->x1 = x1;
+    this->x2 = x2;
+    this->y1 = y1;
+    this->y2 = y2;
+  }
+};
 
-    double dist(double x1, double y1, double x2, double y2)
-    {
-        return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-    }
+double dist(double x1, double y1, double x2, double y2) {
+  return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
 
-    double dist(point_2D p1, point_2D p2)
-    {
-        return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-    }
+double dist(point_2D p1, point_2D p2) {
+  return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+}
 
-    // returns angle from -PI to PI
-    double normalizeAngle(double angle)
-    {
-        if (angle < -M_PI)
-        {
-            while (angle < -M_PI)
-                angle += 2 * M_PI;
-        }
-        else if (angle > M_PI)
-        {
-            while (angle > -M_PI)
-                angle -= 2 * M_PI;
-        }
-        return angle;
-    }
+// returns angle from -PI to PI
+double normalizeAngle(double angle) {
+  if (angle < -M_PI) {
+    while (angle < -M_PI) angle += 2 * M_PI;
+  } else if (angle > M_PI) {
+    while (angle > -M_PI) angle -= 2 * M_PI;
+  }
+  return angle;
+}
 
 }  // namespace footstep_planning
 #endif
