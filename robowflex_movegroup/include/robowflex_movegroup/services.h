588f7adffcf711c9b993c7d9cdab850ee4a31e37
Zachary Kingston <zak@rice.edu>
2018-07-26 15:14:08 -0500

    API for move group helper

--- robowflex_movegroup/include/robowflex_movegroup/services.h
+++ robowflex_movegroup/include/robowflex_movegroup/services.h
@@ -3,53 +3,89 @@
 #ifndef ROBOWFLEX_MOVEGROUP_SERVICES_
 #define ROBOWFLEX_MOVEGROUP_SERVICES_
 
+#include <actionlib/client/simple_action_client.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+
 namespace robowflex
 {
     namespace movegroup
     {
-        /** Creates instances of the move_group interface for things to use. */
-        // class MoveGroupPool
-        // {
-        //     /** get movegroup by group */
-        // };
-
-        /** Executes a motion plan by forwarding it to movegroup */
-        // class MoveGroupExecutor
-        // {
-        //     /** execute */
-        // };
-
-        /** Gets robot from movegroup configuration. has ability to push state to move_group */
-        // class MoveGroupRobot : public Robot
-        // {
-        //     /** push (state) */
-        // };
-
-        /** Gets the scene from the planning scene monitor. has ability to push state to move_group */
-        // class MoveGroupScene : public Scene
-        // {
-        //     /** pull / push */
-        // };
-
-        /** Does planning by forwarding request to move_group. */
-        // class MoveGroupPlanner : public Planner
-        // {
-        // public:
-        //     MoveGroupPlanner();
-        //     ~MoveGroupPlanner();
-
-        //     planning_interface::MotionPlanResponse
-        //     plan(const SceneConstPtr &scene, const planning_interface::MotionPlanRequest &request) override;
-
-        //     const std::vector<std::string> getPlannerConfigs() const override;
-        // };
-
-        /** Listens for all incoming motion planning requests to move_group and does a call back.
+        /** \brief A helper class that allows for pulling and pushing of scenes, robots, and trajectories to
+         *  move group.
          */
-        // class MoveGroupMonitor
-        // {
-        //     /** setCallback in consturctor??? */
-        // };
+        class MoveGroupHelper
+        {
+        public:
+            typedef std::function<void(
+                const SceneConstPtr &scene, const moveit_msgs::MotionPlanRequest &request,
+                moveit_msgs::MoveItErrorCodes code,
+                const robot_trajectory::RobotTrajectoryConstPtr &trajectory, double time)>
+                ResultCallback;
+
+            /** \brief Constructor. Sets up service clients.
+             */
+            MoveGroupHelper(const std::string &move_group = MOVE_GROUP);
+
+            /** \brief Destructor. Cleans up services.
+             */
+            ~MoveGroupHelper();
+
+            /** \brief Sets a callback function that is called whenever a motion plan request is serviced by
+             *  move group. Callback is called with the planning scene, planning request, resulting error
+             * code, and the trajectory and planning time upon success. \param[in] callback Callback function
+             * to call upon move group requests.
+             */
+            void setResultCallback(const ResultCallback &callback);
+
+            /** \brief Executes a planned trajectory through move group.
+             *  \param[in] group Planning group to plan for.
+             *  \param[in] path Path to execute.
+             */
+            void executeTrajectory(const std::string &group,
+                                   const robot_trajectory::RobotTrajectory &path) const;
+
+            /** \brief Pulls the current robot state from move group.
+             *  \param[out] robot Robot whose state to set.
+             */
+            void pullState(RobotPtr &robot) const;
+
+            /** \brief Pulls the current planning scene from move group.
+             *  \param[out] scene Scene to set to the current scene observed by move group.
+             */
+            void pullScene(ScenePtr &scene) const;
+
+            /** \brief Pushes the current planning scene to move group.
+             *  \param[in] scene Scene to use to set move group's current scene.
+             */
+            void pushScene(const SceneConstPtr &scene) const;
+
+        private:
+            /** \brief Callback function for a move group goal.
+             *  \param[in] Goal message.
+             */
+            void moveGroupGoalCallback(const moveit_msgs::MoveGroupActionGoal &msg);
+
+            /** \brief Callback function for a move group result.
+             *  \param[in] Result message.
+             */
+            void moveGroupResultCallback(const moveit_msgs::MoveGroupActionResult &msg);
+
+            ros::NodeHandle nh_;          ///< Node handle.
+            ros::Subscriber goal_sub_;    ///< Move group goal subscriber.
+            ros::Subscriber result_sub_;  ///< Move group result subscriber.
+            ros::ServiceClient gpsc_;     ///< Get planning scene service.
+            ros::ServiceClient apsc_;     ///< Apply planning scene service.
+
+            actionlib::SimpleActionClient<moveit_msgs::ExecuteTrajectoryAction> eac_;  ///< Execute trajectory
+                                                                                       ///< client.
+
+            ResultCallback callback_;  ///< Callback function for move group results.
+
+            static const std::string MOVE_GROUP;   ///< Name of move_group namespace.
+            static const std::string GET_SCENE;    ///< Name of get scene service.
+            static const std::string APPLY_SCENE;  ///< Name of apply scene service.
+            static const std::string EXECUTE;      ///< Name of execute trajectory service.
+        };
     }  // namespace movegroup
 }  // namespace robowflex
 
--- robowflex_movegroup/src/services.cpp
+++ robowflex_movegroup/src/services.cpp
@@ -1,3 +1,90 @@
 /* Author: Zachary Kingston */
 
 #include <robowflex_movegroup/services.h>
+
+const std::string MoveGroupHelper::MOVE_GROUP{"/move_group"};
+const std::string MoveGroupHelper::GET_SCENE{"get_planning_scene"};
+const std::string MoveGroupHelper::APPLY_SCENE{"apply_planning_scene"};
+const std::string MoveGroupHelper::EXECUTE{"execute_trajectory"};
+
+MoveGroupHelper::MoveGroupHelper(const std::string &move_group) : nh_("~")
+{
+    gpsc_ = nh_.serviceClient<moveit_msgs::GetPlanningScene>(GET_SCENE, true);
+    apsc_ = nh_.serviceClient<moveit_msgs::ApplyPlanningScene>(APPLY_SCENE, true);
+
+    eac_ = actionlib::SimpleActionClient<moveit_msgs::ExecuteTrajectoryAction>(
+        nh_, EXECUTE, false);
+
+    goal_sub_ = nh_.subscribe(move_group + "/goal", 10, &MoveGroupHelper::moveGroupGoalCallback, this);
+    result_sub_ = nh_.subscribe(move_group + "/result", 10, &MoveGroupHelper::moveGroupResultCallback, this);
+}
+
+MoveGroupHelper::~MoveGroupHelper()
+{
+}
+
+void MoveGroupHelper::setGoalCallback(const GoalCallback &callback)
+{
+    callback_ = callback;
+}
+
+void MoveGroupHelper::executeTrajectory(const std::string &group,
+                                        const robot_trajectory::RobotTrajectory &path) const
+{
+}
+
+void MoveGroupHelper::pullState(RobotPtr &robot) const
+{
+}
+
+void MoveGroupHelper::pullScene(ScenePtr &scene) const
+{
+}
+
+void MoveGroupHelper::pushScene(const SceneConstPtr &scene) const
+{
+}
+
+void moveGroupGoalCallback(const moveit_msgs::MoveGroupActionGoal &msg)
+{
+    // const std::string &id = msg.goal_id.id;
+    // ROS_INFO("Intercepted request goal ID: `%s`", id.c_str());
+
+    // requests_.emplace(std::piecewise_construct,  //
+    //                   std::forward_as_tuple(id), std::forward_as_tuple(retrieveScene(), msg.goal.request));
+}
+
+void moveGroupResultCallback(const moveit_msgs::MoveGroupActionResult &msg)
+{
+    // const std::string &id = msg.status.goal_id.id;
+
+    // auto request = requests_.find(id);
+    // if (request == requests_.end())
+    // {
+    //     ROS_WARN("Intercepted unknown request response ID: `%s`", id.c_str());
+    //     return;
+    // }
+
+    // ROS_WARN("Intercepted request response ID: `%s`", id.c_str());
+
+    // YAML::Node node;
+
+    // if (msg.result.error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
+    // {
+    //     node["success"] = "true";
+    //     node["trajectory"] = msg.result.planned_trajectory;
+    // }
+    // else
+    //     node["success"] = "false";
+
+    // ros::Time time = ros::Time::now();
+    // node["id"] = id;
+    // node["time"] = time;
+    // node["scene"] = request->second.first;
+    // node["request"] = request->second.second;
+
+    // IO::YAMLToFile(node, "~/robowflex_tapedeck/" + to_iso_string(time.toBoost()) + ".yml");
+    // ROS_WARN("  Wrote YAML for ID: `%s`", id.c_str());
+
+    // requests_.erase(request);
+}
