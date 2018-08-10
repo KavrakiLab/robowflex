aa4581cce6c72dea048e60923de4c08d40f7bf2e
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2018-08-10 00:18:58 -0500

    Lunar/Kinetic Version numbers were incorrect.

--- robowflex_library/include/robowflex_library/macros.h
+++ robowflex_library/include/robowflex_library/macros.h
@@ -16,11 +16,11 @@
 /** \brief Tests if the ROS version is Indigo or higher. */
 #define ROBOWFLEX_AT_LEAST_INDIGO ROS_VERSION_MINIMUM(1, 11, 0)
 
-/** \brief Tests if the ROS version is Lunar or higher. */
-#define ROBOWFLEX_AT_LEAST_LUNAR ROS_VERSION_MINIMUM(1, 12, 0)
-
 /** \brief Tests if the ROS version is Kinetic or higher. */
-#define ROBOWFLEX_AT_LEAST_KINETIC ROS_VERSION_MINIMUM(1, 13, 0)
+#define ROBOWFLEX_AT_LEAST_KINETIC ROS_VERSION_MINIMUM(1, 12, 0)
+
+/** \brief Tests if the ROS version is Lunar or higher. */
+#define ROBOWFLEX_AT_LEAST_LUNAR ROS_VERSION_MINIMUM(1, 13, 0)
 
 /** \brief Tests if the ROS version is Melodic or higher. */
 #define ROBOWFLEX_AT_LEAST_MELODIC ROS_VERSION_MINIMUM(1, 14, 0)
@@ -28,7 +28,7 @@
 /** \brief Tests if boost is at least version 1.64 */
 #define IS_BOOST_164 BOOST_VERSION >= 106400
 
-#if ROBOWFLEX_AT_LEAST_LUNAR
+#if ROBOWFLEX_AT_LEAST_KINETIC
 /** \brief Sets flow style for YAML nodes. */
 #define ROBOWFLEX_YAML_FLOW(n) n.SetStyle(YAML::EmitterStyle::Flow);
 #else
