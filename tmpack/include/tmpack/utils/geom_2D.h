01b8af4b8d3034f7c2746b40fbe5383b601ac188
Zachary Kingston <zak@rice.edu>
2018-08-14 12:18:52 -0500

    Updated License Year in TMPack

--- tmpack/include/tmpack/utils/geom_2D.h
+++ tmpack/include/tmpack/utils/geom_2D.h
@@ -1,7 +1,7 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
 *
-*  Copyright (c) 2014, Rice University
+*  Copyright (c) 2018, Rice University
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
