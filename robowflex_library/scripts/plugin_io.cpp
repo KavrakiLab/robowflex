0d5c57e17b84eb301802aaf18e4f384fb64f5f83
Zak Kingston <zak@rice.edu>
2018-08-24 16:10:37 -0500

    Fixed plugin loading with bad base package

--- robowflex_library/scripts/plugin_io.cpp
+++ robowflex_library/scripts/plugin_io.cpp
@@ -2,15 +2,23 @@
 
 #include <moveit/planning_request_adapter/planning_request_adapter.h>
 
+#include <robowflex_library/util.h>
 #include <robowflex_library/io/plugin.h>
 
 using namespace robowflex;
 
 int main(int argc, char **argv)
 {
-    auto &manager = IO::PluginManager::getInstance();
-    auto plugin = manager.load<planning_request_adapter::PlanningRequestAdapter>(  //
-        "default_planner_request_adapters/AddTimeParameterization");
+    ROS ros(argc, argv);
+
+    auto plugin1 = IO::PluginManager::load<planning_request_adapter::PlanningRequestAdapter>(  //
+        "moveit_core", "default_planner_request_adapters/AddTimeParameterization");
+
+    auto plugin2 = IO::PluginManager::load<planning_request_adapter::PlanningRequestAdapter>(  //
+        "moveit_core", "default_planner_request_adapters/AddTimeParameterization");
+
+    auto plugin2 = IO::PluginManager::load<planning_request_adapter::PlanningRequestAdapter>(  //
+        "moveit_core", "default_planner_request_adapters/AddTimeParameterization");
 
     return 0;
 }
