fd25ac7a467de2546146dd009c073c402f5612b2
Zak Kingston <zak@rice.edu>
2019-01-31 11:12:49 -0600

    documentation + fetch demo runs in reasonable time

--- robowflex_library/include/robowflex_library/adapter.h
+++ robowflex_library/include/robowflex_library/adapter.h
@@ -9,7 +9,11 @@
 
 namespace robowflex
 {
-    /** \brief A pose (point in SE(3)) used in various functions. Defined from what \e MoveIt! uses. */
+    /** \brief A pose (point in SE(3)) used in various functions. Defined from what \e MoveIt! uses.
+     *
+     *  Either an Eigen::Affine3d or Eigen::Isometry3d. These are both transforms, and thus code using a
+     *  RobotPose will generally work for both.
+     */
     using RobotPose = std::decay<                                      //
         decltype(                                                      //
             std::declval<moveit::core::Transforms>().getTransform("")  //
