6f381dfe0feed8202850e6d3425a69466e1a48c3
Zak Kingston <zak@rice.edu>
2020-01-10 14:17:53 -0600

    static transform rework

--- robowflex_library/include/robowflex_library/io/broadcaster.h
+++ robowflex_library/include/robowflex_library/io/broadcaster.h
@@ -42,20 +42,45 @@ namespace robowflex
              */
             void stop();
 
+            /** \brief Add a new static transform to the broadcaster.
+             *  \param[in] name Name of transform (used for removal)
+             *  \param[in] base Base frame of transform.
+             *  \param[in] target Target frame of transform.
+             *  \param[in] tf The transform from \a base to \a target.
+             */
+            void addStaticTransform(const std::string &name, const std::string &base,
+                                    const std::string &target, const RobotPose &tf = RobotPose::Identity());
+
+            /** \brief Remove a named static transform
+             *  \param[in] name Name of transform to remove.
+             */
+            void removeStaticTransform(const std::string &name);
+
         private:
             /** \brief Send out the TF and joint information.
              */
             void update();
 
+            RobotConstPtr robot_;                  ///< Robot being published.
             const std::string base_;               ///< Base frame to use.
             bool active_{false};                   ///< Is thread active?
             bool done_{false};                     ///< Is thread done?
             unsigned int rate_{10};                ///< Times per second to send out.
             std::unique_ptr<std::thread> thread_;  ///< Worker thread.
-            RobotConstPtr robot_;                  ///< Robot being published.
             ros::NodeHandle nh_;                   ///< Handle for publishing.
             tf2_ros::TransformBroadcaster tf2br_;  ///< TF2 broadcaster
             ros::Publisher state_pub_;             ///< State publisher.
+
+            /** \brief Information for a static transform.
+             */
+            struct StaticTransform
+            {
+                std::string base;    ///< Base frame
+                std::string target;  ///< Target frame
+                RobotPose tf;        ///< Transform
+            };
+
+            std::map<std::string, StaticTransform> static_;  ///< Static transforms.
         };
     }  // namespace IO
 }  // namespace robowflex
