ab72d7ba50953ee939e6238bcba97826f17b7db3
Zachary Kingston <zak@rice.edu>
2020-07-16 09:36:11 -0500

    script for checking format

--- .travis/check-format.sh
+++ .travis/check-format.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-find .. -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -not -path "../.docs/*" -exec clang-format -style=file -output-replacements-xml {} \;
+find .. -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -not -path "../.docs/*" -exec clang-format -style=file -output-replacements-xml {} \; | grep "<replacement "
+RESULT="$?"
+
+if [ "$RESULT" = "0" ]; then
+    false
+else
+    true
+fi
