8290e319318a518255dac7ba28cc74fbc8bbefca
Zachary Kingston <zak@rice.edu>
2020-07-20 13:51:25 -0500

    tidy script updates

--- tidy.sh
+++ tidy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+# Note: this script requires the local ROS environment to be sourced.
+
+function generate_compile_commands()
+{
+    mkdir .build-tmp
+    pushd .build-tmp
+    cmake ../$1/. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    commands=`realpath compile_commands.json`
+    popd
+}
+
+function tidy()
+{
+	  run-clang-tidy -fix -header-filter=".*" -p $(dirname $commands)
+}
+
 cd "${0%/*}"
-find . -regex '.*\.\(cpp\|h\)' -not -path ".docs/*" -exec clang-tidy -fix -fix-errors {} -- -std=c++11 \;
+generate_compile_commands robowflex_library
+tidy
