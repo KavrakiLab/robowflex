8fcba6e0a2189a96c0455b70277fc7fd852122d4
Constantinos <chamzask@gmail.com>
2021-01-15 19:13:23 +0200

    Add Attached Collision Objects to Scene (Partially Addresses #147) (#148)

--- robowflex_visualization/robowflex_visualization/scene.py
+++ robowflex_visualization/robowflex_visualization/scene.py
@@ -34,6 +34,11 @@ class Scene:
             for co in self.yaml["world"]["collision_objects"]:
                 self.add_collision_object(co)
 
+        if self.yaml["robot_state"]:
+            if "attached_collision_objects" in self.yaml["robot_state"].keys(): 
+               for cao in self.yaml["robot_state"]["attached_collision_objects"]:
+                   self.add_collision_object(cao["object"])
+
     ## @brief Adds a collision object (moveit_msgs::CollisionObject) to the scene.
     #
     #  @param cos Collision object.
