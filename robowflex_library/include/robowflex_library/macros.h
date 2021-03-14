4ca0cceb638c0c7817ccc8a77a72dcce72a0ab7a
Constantinos <chamzask@gmail.com>
2021-03-14 14:29:02 -0500

    Fixed macro bug (#161)

--- robowflex_library/include/robowflex_library/macros.h
+++ robowflex_library/include/robowflex_library/macros.h
@@ -32,7 +32,7 @@
 
 /** \brief Tests if the ROS version is not further than Melodic */
 #define ROBOWFLEX_AT_MOST_MELODIC                                                                            \
-    ROS_VERSION_GE(1, 14, 0, ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH)
+    ROS_VERSION_GE(1, 15, 0, ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH)
 
 /** \brief Tests if boost is at least version 1.64 */
 #define IS_BOOST_164 (BOOST_VERSION >= 106400u)
