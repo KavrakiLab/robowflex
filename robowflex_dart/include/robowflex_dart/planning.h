d5411a6d28cfb168f5afc60a65f990a671201c9b
tianyang <1045380922@qq.com>
2021-03-16 17:24:49 -0500

    fix linting error

--- robowflex_dart/include/robowflex_dart/planning.h
+++ robowflex_dart/include/robowflex_dart/planning.h
@@ -168,7 +168,8 @@ namespace robowflex
             double distanceGoal(const ompl::base::State *state) const override;
 
 
-            /** \brief Get the TSR set of this goal region
+            /** \brief Get the TSR set of this goal region.
+             *  \return The TSR set.
              */
             TSRSetPtr getTSRSet();
 
