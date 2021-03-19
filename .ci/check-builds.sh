fe7d825469cfff0c38396c63ca2c2adb6798ab9d
Zachary Kingston <kingston.zak@gmail.com>
2021-03-18 19:33:44 -0500

    Cleaning up macros, more refined check for collision world

---
+++ .ci/check-builds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "${0%/*}"
+pushd ../.docker
+./indigo/build-indigo.sh
+./kinetic/build-kinetic.sh
+./melodic/build-melodic.sh
+./melodic-source/build-melodic-source.sh
+./noetic/build-noetic.sh
+./tesseract/build-tesseract.sh
+popd
--- robowflex_library/src/scene.cpp
+++ robowflex_library/src/scene.cpp
@@ -410,7 +410,7 @@ double Scene::distanceToCollision(const robot_state::RobotStatePtr &state) const
 
 double Scene::distanceToObject(const robot_state::RobotStatePtr &state, const std::string &object) const
 {
-#if ROBOWFLEX_AT_LEAST_KINETIC and ROBOWFLEX_AT_MOST_MELODIC
+#if not ROBOWFLEX_MOVEIT_COLLISION_ENV
     if (not hasObject(object))
     {
         ROS_ERROR("World does not have object `%s`", object.c_str());
@@ -455,7 +455,7 @@ double Scene::distanceToObject(const robot_state::RobotStatePtr &state, const st
 
 double Scene::distanceBetweenObjects(const std::string &one, const std::string &two) const
 {
-#if ROBOWFLEX_AT_LEAST_KINETIC and ROBOWFLEX_AT_MOST_MELODIC
+#if not ROBOWFLEX_MOVEIT_COLLISION_ENV
     // Early terminate if they are the same
     if (one == two)
         return 0.;
