7cf59131c7ba103a05095f3ec21e33fba4c1bcba
Mark Moll <mmoll@rice.edu>
2021-05-03 10:32:43 -0700

    Collision Checking Version Checking Fixes (#192)
    * make version consistent with package.xml version
    
    * moveit version check fix for those using melodic with a newer version of moveit
    
    * formatting fixes
    
    Co-authored-by: Zachary Kingston <kingston.zak@gmail.com>

--- robowflex_visualization/setup.py
+++ robowflex_visualization/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'robowflex_visualization',
-    version = '0.1.0',
+    version = '1.1.0',
     description = 'Robowflex: Blender Visualization Tools',
     author = 'Zachary Kingston',
     author_email = 'zak@rice.edu',
