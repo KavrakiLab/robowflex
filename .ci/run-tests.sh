8f9f33dbd249102e0eee2bd09c75c5926339e017
Zachary Kingston <kingston.zak@gmail.com>
2021-05-04 20:22:23 -0500

    fixed container name

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -3,7 +3,7 @@
 function check-docker-tests()
 {
     local container=$1
-    docker run -t --network=host --name indigo_test -d robowflex/${container}
+    docker run -t --network=host --name ${container}_test -d robowflex/${container}
     docker exec ${container}_test /bin/bash -c "source ros_entrypoint.sh; nohup roscore &> /dev/null &"
     docker exec ${container}_test /bin/bash -c "source ros_entrypoint.sh; cd /ws/; catkin run_tests robowflex_library --no-deps --limit-status-rate 0.001 --no-notify; catkin_test_results"
     local result=$?
