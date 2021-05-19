d4d962e6435067508d36d896206fe7e47fb8f9cf
Zachary Kingston <kingston.zak@gmail.com>
2021-05-19 16:06:32 -0500

    small script update to baxter multi

--- robowflex_library/scripts/baxter_multi_target.cpp
+++ robowflex_library/scripts/baxter_multi_target.cpp
@@ -28,42 +28,44 @@ int main(int argc, char **argv)
     // Startup ROS
     ROS ros(argc, argv);
 
-    // Create the default Fetch robot.
+    // Create a Baxter robot.
     auto baxter = std::make_shared<Robot>("baxter");
-
-    // Initialize baxter from robowflex_resources.
     baxter->initializeFromYAML("package://robowflex_resources/baxter.yml");
     baxter->loadKinematics(LEFT_ARM);
     baxter->loadKinematics(RIGHT_ARM);
 
+    // Visualize the Baxter robot
     IO::RVIZHelper rviz(baxter);
     rviz.visualizeCurrentState();
-    RBX_INFO("Default robot state is visualized. \n Press enter to continue...");
+    RBX_INFO("Default robot state is visualized. Press enter to continue...");
     std::cin.ignore();
 
-    // an EE offset that makes the arm point upwards.
-    auto ee_offset = TF::createPoseXYZ(0, 0, -1.3, constants::pi, 0, 0);
+    // EE offset that makes the arm point upwards.
+    const auto &ee_offset = TF::createPoseXYZ(0, 0, -1.3, constants::pi, 0, 0);
+
     // Use IK to shift robot arm over by desired amount.
-    RobotPose goal_pose_left = baxter->getLinkTF(LEFT_EE) * ee_offset;
-    RobotPose goal_pose_right = baxter->getLinkTF(RIGHT_EE) * ee_offset;
+    const RobotPose &goal_pose_left = baxter->getLinkTF(LEFT_EE) * ee_offset;
+    const RobotPose &goal_pose_right = baxter->getLinkTF(RIGHT_EE) * ee_offset;
 
     // Visualizing the target poses.
     rviz.addTransformMarker("pose_right", "map", goal_pose_right);
     rviz.addTransformMarker("pose_left", "map", goal_pose_left);
     rviz.updateMarkers();
-    RBX_INFO("Target IK poses are visualized. \n Press enter to continue...");
+    RBX_INFO("Target IK poses are visualized. Press enter to continue...");
     std::cin.ignore();
 
-    const auto ik_query = Robot::IKQuery(BOTH_ARMS, {goal_pose_left, goal_pose_right}, {LEFT_EE, RIGHT_EE});
+    const auto &query = Robot::IKQuery(BOTH_ARMS, {goal_pose_left, goal_pose_right}, {LEFT_EE, RIGHT_EE});
 
-    if (not baxter->setFromIK(ik_query))
+    if (not baxter->setFromIK(query))
     {
-        RBX_INFO("IK query failed!");
-        exit(-1);
+        RBX_ERROR("IK query failed!");
+        return 1;
     }
+
+    // Visualize resulting state.
     rviz.visualizeCurrentState();
     RBX_INFO("Solution to IK is visualized. Press enter to exit...");
     std::cin.ignore();
 
-    exit(0);
+    return 0;
 }
