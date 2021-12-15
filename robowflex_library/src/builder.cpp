0afb617a15ec575e56ea1e5b1b3e69e7843bd2d3
Zachary Kingston <kingston.zak@gmail.com>
2021-12-15 17:46:38 -0600

    Method to Precompute Goal Configurations (#270)
    * Precompute goal set
    
    * fixed
    
    * changed to in-place precomputation
    
    * compilation fixes
    
    * manager
    
    * fix

--- robowflex_library/src/builder.cpp
+++ robowflex_library/src/builder.cpp
@@ -1,10 +1,14 @@
 /* Author: Zachary Kingston */
 
+#include <moveit/constraint_samplers/constraint_sampler.h>
+#include <moveit/constraint_samplers/constraint_sampler_manager.h>
+#include <moveit/constraint_samplers/default_constraint_samplers.h>
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/robot_state/conversions.h>
 
 #include <robowflex_library/builder.h>
 #include <robowflex_library/constants.h>
+#include <robowflex_library/random.h>
 #include <robowflex_library/geometry.h>
 #include <robowflex_library/io.h>
 #include <robowflex_library/io/yaml.h>
@@ -403,6 +407,36 @@ void MotionRequestBuilder::setGoalRegion(const std::string &ee_name, const std::
 {
     clearGoals();
     addGoalRegion(ee_name, base_name, pose, geometry, orientation, tolerances);
+}
+
+void MotionRequestBuilder::precomputeGoalConfigurations(std::size_t n_samples, const ScenePtr &scene,
+                                                        const ConfigurationValidityCallback &callback)
+{
+    // Allocate samplers for each region
+    constraint_samplers::ConstraintSamplerManager manager;
+    std::vector<constraint_samplers::ConstraintSamplerPtr> samplers;
+    for (const auto &goal : request_.goal_constraints)
+    {
+        samplers.emplace_back(manager.selectSampler(scene->getSceneConst(), group_name_, goal));
+        samplers.back()->setGroupStateValidityCallback(scene->getGSVCF(false));
+    }
+
+    clearGoals();
+
+    // Clone start
+    robot_state::RobotState state = *robot_->getScratchStateConst();
+
+    // Sample n_samples to add to new request
+    std::size_t n = n_samples;
+    while (n)
+    {
+        auto sampler = RNG::uniformSample(samplers);
+        if (sampler->sample(state) and (not callback or callback(state)))
+        {
+            addGoalConfiguration(state);
+            n--;
+        }
+    }
 }
 
 void MotionRequestBuilder::clearGoals()
