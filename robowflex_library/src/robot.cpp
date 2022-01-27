fd50b7c6f519a84e472c3e13d39e76e7524055e0
Constantinos <chamzask@gmail.com>
2022-01-27 11:10:44 -0600

    Fix external validation bug with IK  (#272)
    * Changed script to illustrate baxter ik bug
    
    * Fixes #271
    
    * Addressed comments

--- robowflex_library/src/robot.cpp
+++ robowflex_library/src/robot.cpp
@@ -813,10 +813,11 @@ void Robot::IKQuery::getMessage(const std::string &base_frame, moveit_msgs::Cons
 kinematic_constraints::KinematicConstraintSetPtr Robot::IKQuery::getAsConstraints(const Robot &robot) const
 {
     moveit_msgs::Constraints msg;
-    getMessage(robot.getSolverBaseFrame(group), msg);
+    const auto &root = robot.getModelConst()->getRootLink()->getName();
+    getMessage(root, msg);
 
     auto constraints = std::make_shared<kinematic_constraints::KinematicConstraintSet>(robot.getModelConst());
-    moveit::core::Transforms none(robot.getModelConst()->getModelFrame());
+    moveit::core::Transforms none(root);
 
     constraints->add(msg, none);
 
@@ -840,6 +841,12 @@ bool Robot::setFromIK(const IKQuery &query)
 
 bool Robot::setFromIK(const IKQuery &query, robot_state::RobotState &state) const
 {
+    // copy query for unconstness
+    std::vector<std::string> tips(query.tips);
+
+    if (tips[0].empty())
+        tips = getSolverTipFrames(query.group);
+
     const robot_model::JointModelGroup *jmg = model_->getJointModelGroup(query.group);
     const auto &gsvcf =
         (query.scene) ? query.scene->getGSVCF(query.verbose) : moveit::core::GroupStateValidityCallbackFn{};
@@ -860,19 +867,12 @@ bool Robot::setFromIK(const IKQuery &query, robot_state::RobotState &state) cons
         query.sampleRegions(targets);
 
 #if ROBOWFLEX_AT_LEAST_MELODIC
-        // Multi-tip IK. Will delegate automatically to RobotState::setFromIKSubgroups() if the kinematics
+        // Multi-tip IK: Will delegate automatically to RobotState::setFromIKSubgroups() if the kinematics
         // solver doesn't support multi-tip queries.
-        if (targets.size() > 1)
-            success = state.setFromIK(jmg, targets, query.tips, query.timeout, gsvcf, query.options);
-        // Single-tip IK.
-        else
-            success = state.setFromIK(jmg, targets[0], query.timeout, gsvcf, query.options);
-
-#else  // attempts was a prior field that was deprecated in melodic
-        if (targets.size() > 1)
-            success = state.setFromIK(jmg, targets, query.tips, 1, query.timeout, gsvcf, query.options);
-        else
-            success = state.setFromIK(jmg, targets[0], 1, query.timeout, gsvcf, query.options);
+        success = state.setFromIK(jmg, targets, tips, query.timeout, gsvcf, query.options);
+#else
+        // attempts was a prior field that was deprecated in melodic
+        success = state.setFromIK(jmg, targets, tips, 1, query.timeout, gsvcf, query.options);
 #endif
 
         if (evaluate)
